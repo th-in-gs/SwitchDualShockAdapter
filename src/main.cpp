f1e9e0531e52da2e25ecadab524d0b5bc60bb6f2
Jamie Montgomerie <jamie@montgomerie.net>
2022-12-03 23:07:28 -0800

    Change the way we're including V-USB so that it's symlinked from a pristine clone in a top level folder. This allows us to reach the osccal routines, which are in a separate folder in the V-USB hierarchy.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <usbdrv.h>
+#include <usbdrv/usbdrv.h>
 
 void setup()
 {
--- v-usb_platformio_helper.py
+++ v-usb_platformio_helper.py
@@ -1,11 +1,11 @@
 Import("env")
 
 # V-USB has a '.asm' file in the usbdrv folder that PlatformIO will try to 
-# compile, but it's really just for "The IAR compiler/assembler system".
-# We'll make it be skipped.
+# compile - but it's really just for the "IAR compiler/assembler system" and 
+# will just cause errors. We'll make it be skipped.
 def skip_file(node):
     return None
-env.AddBuildMiddleware(skip_file, "*/lib/usbdrv/*.asm")
+env.AddBuildMiddleware(skip_file, "*/lib/v-usb/*.asm")
 
 # Make sure V-USB can find its config file, which we've placed inside the 
 # top-level include folder.
@@ -15,4 +15,4 @@ def add_usbdrv_include(node):
         CFLAGS=env["CFLAGS"] + ["-Iinclude"],
         ASFLAGS=env["ASFLAGS"] + ["-Iinclude"],
     )
-env.AddBuildMiddleware(add_usbdrv_include, "*/lib/usbdrv/*.[chS]")
+env.AddBuildMiddleware(add_usbdrv_include, "*/lib/v-usb/*.[chS]")
