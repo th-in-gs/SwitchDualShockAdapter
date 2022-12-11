41652861a9708e09201929d3912018ec249958f4
Jamie Montgomerie <jamie@montgomerie.net>
2022-12-10 20:26:42 -0800

    Abortive attempts at Pro Controller communication

--- include/usbconfig.h
+++ include/usbconfig.h
@@ -74,7 +74,7 @@ section at the end of this file).
 
 /* --------------------------- Functional Range ---------------------------- */
 
-#define USB_CFG_HAVE_INTRIN_ENDPOINT    0
+#define USB_CFG_HAVE_INTRIN_ENDPOINT    1
 /* Define this to 1 if you want to compile a version with two endpoints: The
  * default control endpoint 0 and an interrupt-in endpoint (any other endpoint
  * number).
@@ -218,7 +218,7 @@ section at the end of this file).
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+#define  USB_CFG_VENDOR_ID       0x7E, 0x05
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
@@ -227,7 +227,7 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define  USB_CFG_DEVICE_ID       0xdc, 0x05 /* = 0x05dc = 1500 */
+#define  USB_CFG_DEVICE_ID       0x09, 0x20 
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
@@ -238,11 +238,12 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define USB_CFG_DEVICE_VERSION  0x00, 0x01
+#define USB_CFG_DEVICE_VERSION  0x00, 0x02
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'o', 'b', 'd', 'e', 'v', '.', 'a', 't'
-#define USB_CFG_VENDOR_NAME_LEN 8
+// Nintendo Co., Ltd
+#define USB_CFG_VENDOR_NAME     'N','i','n','t','e','n','d','o',' ','C','o','.',',',' ','L','t','d'
+#define USB_CFG_VENDOR_NAME_LEN 17
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -251,14 +252,16 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'T', 'e', 'm', 'p', 'l', 'a', 't', 'e'
-#define USB_CFG_DEVICE_NAME_LEN 8
+// Pro Controller
+#define USB_CFG_DEVICE_NAME     'P','r','o',' ','C','o','n','t','r','o','l','l','e','r'
+#define USB_CFG_DEVICE_NAME_LEN 14
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
  */
-/*#define USB_CFG_SERIAL_NUMBER   'N', 'o', 'n', 'e' */
-/*#define USB_CFG_SERIAL_NUMBER_LEN   0 */
+// 000000000001
+#define USB_CFG_SERIAL_NUMBER   '0','0','0','0','0','0','0','0','0','0','0','1'
+#define USB_CFG_SERIAL_NUMBER_LEN   12
 /* Same as above for the serial number. If you don't want a serial number,
  * undefine the macros.
  * It may be useful to provide the serial number through other means than at
@@ -266,12 +269,12 @@ section at the end of this file).
  * to fine tune control over USB descriptors such as the string descriptor
  * for the serial number.
  */
-#define USB_CFG_DEVICE_CLASS        0xff    /* set to 0 if deferred to interface */
+#define USB_CFG_DEVICE_CLASS        0    /* set to 0 if deferred to interface */
 #define USB_CFG_DEVICE_SUBCLASS     0
 /* See USB specification if you want to conform to an existing device class.
  * Class 0xff is "vendor specific".
  */
-#define USB_CFG_INTERFACE_CLASS     0   /* define class here if not at device level */
+#define USB_CFG_INTERFACE_CLASS     3   /* define class here if not at device level */
 #define USB_CFG_INTERFACE_SUBCLASS  0
 #define USB_CFG_INTERFACE_PROTOCOL  0
 /* See USB specification if you want to conform to an existing device class or
@@ -279,7 +282,7 @@ section at the end of this file).
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-/* #define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    42 */
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    203
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,82 @@ extern "C" {
     uint8_t lastTimer0Value = 0;
 }
 
+#include "descriptors.h"
+
+
+static inputReport30_t reports[2] = {0};
+volatile static uint8_t reportReadIndex = 0;
+
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
-    return 0;
+    usbRequest_t* rq = (usbRequest_t*)data;
+
+    /* The following requests are never used. But since they are required by
+     * the specification, we implement them in this example.
+     */
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) { /* class request type */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT) {
+            if(rq->wValue.bytes[0] == 0x30) {
+                /* we only have one report type, so don't look at wValue */
+                usbMsgPtr = (typeof(usbMsgPtr))(&reports[reportReadIndex]);
+                return sizeof(inputReport30_t);
+            }
+        }
+    } else {
+        /* no vendor specific requests implemented */
+    }
+    return 0; /* default for not implemented requests: return no data back to host */
+}
+
+static boolean sLedIsOn = false;
+
+static void prepareReport()
+{
+    uint8_t nextReportIndex = reportReadIndex == 1 ? 0 : 1;
+    inputReport30_t *report = &reports[nextReportIndex];
+    report->reportId = 0x30;
+    report->BTN_JoystickButton1 = sLedIsOn;
+/*
+typedef struct
+{
+  uint8_t  reportId;                                 // Report ID = 0x30 (48) '0'
+                                                     // Collection: CA:Joystick
+  uint8_t  BTN_JoystickButton1 : 1;                  // Usage 0x00090001: Button 1 Primary/trigger, Value = 0 to 1
+  uint8_t  BTN_JoystickButton2 : 1;                  // Usage 0x00090002: Button 2 Secondary, Value = 0 to 1
+  uint8_t  BTN_JoystickButton3 : 1;                  // Usage 0x00090003: Button 3 Tertiary, Value = 0 to 1
+  uint8_t  BTN_JoystickButton4 : 1;                  // Usage 0x00090004: Button 4, Value = 0 to 1
+  uint8_t  BTN_JoystickButton5 : 1;                  // Usage 0x00090005: Button 5, Value = 0 to 1
+  uint8_t  BTN_JoystickButton6 : 1;                  // Usage 0x00090006: Button 6, Value = 0 to 1
+  uint8_t  BTN_JoystickButton7 : 1;                  // Usage 0x00090007: Button 7, Value = 0 to 1
+  uint8_t  BTN_JoystickButton8 : 1;                  // Usage 0x00090008: Button 8, Value = 0 to 1
+  uint8_t  BTN_JoystickButton9 : 1;                  // Usage 0x00090009: Button 9, Value = 0 to 1
+  uint8_t  BTN_JoystickButton10 : 1;                 // Usage 0x0009000A: Button 10, Value = 0 to 1
+  uint8_t  BTN_JoystickButton11 : 1;                 // Usage 0x0009000B: Button 11, Value = 0 to 1
+  uint8_t  BTN_JoystickButton12 : 1;                 // Usage 0x0009000C: Button 12, Value = 0 to 1
+  uint8_t  BTN_JoystickButton13 : 1;                 // Usage 0x0009000D: Button 13, Value = 0 to 1
+  uint8_t  BTN_JoystickButton14 : 1;                 // Usage 0x0009000E: Button 14, Value = 0 to 1
+  uint8_t  : 1;                                      // Pad
+  uint8_t  : 1;                                      // Pad
+                                                     // Collection: CA:Joystick CP:Pointer
+  uint16_t GD_JoystickPointerX;                      // Usage 0x00010030: X, Value = 0 to 65535
+  uint16_t GD_JoystickPointerY;                      // Usage 0x00010031: Y, Value = 0 to 65535
+  uint16_t GD_JoystickPointerZ;                      // Usage 0x00010032: Z, Value = 0 to 65535
+  uint16_t GD_JoystickPointerRz;                     // Usage 0x00010035: Rz, Value = 0 to 65535
+                                                     // Collection: CA:Joystick
+  uint8_t  GD_JoystickHatSwitch : 4;                 // Usage 0x00010039: Hat switch, Value = 0 to 7, Physical = Value x 45 in degrees
+  uint8_t  BTN_JoystickButton15 : 1;                 // Usage 0x0009000F: Button 15, Value = 0 to 1, Physical = Value x 315 in degrees
+  uint8_t  BTN_JoystickButton16 : 1;                 // Usage 0x00090010: Button 16, Value = 0 to 1, Physical = Value x 315 in degrees
+  uint8_t  BTN_JoystickButton17 : 1;                 // Usage 0x00090011: Button 17, Value = 0 to 1, Physical = Value x 315 in degrees
+  uint8_t  BTN_JoystickButton18 : 1;                 // Usage 0x00090012: Button 18, Value = 0 to 1, Physical = Value x 315 in degrees
+  uint8_t  pad_7[52];                                // Pad
+} inputReport30_t;
+*/
+
+    *report = {
+        .reportId = 0x30,
+    };
+
+    reportReadIndex = nextReportIndex;
 }
 
 void setup()
@@ -35,19 +108,35 @@ void setup()
 }
 
 // Call regularly to blink the LED every 1 second.
-static void heartbeat()
+static void ledHeartbeat()
 {
-    static unsigned long lastBlink = 0;
+    static unsigned long lastBeat = 0;
 
     unsigned long timeNow = millis();
-    if(timeNow - lastBlink >= 1000) {
-        lastBlink = timeNow;
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if(timeNow - lastBeat >= 1000) {
+        lastBeat = timeNow;
+        sLedIsOn = !sLedIsOn;
+        digitalWrite(LED_BUILTIN, sLedIsOn);
+    }
+}
+
+static void pollInputHeartbeat() 
+{
+    static unsigned long lastBeat = 0;
+
+    unsigned long timeNow = millis();
+    if(timeNow - lastBeat >= 10) {
+        lastBeat = timeNow;
+        prepareReport();
     }
 }
 
 void loop()
 {
-    heartbeat();
+    ledHeartbeat();
+    pollInputHeartbeat();
     usbPoll();
+    if(usbInterruptIsReady()) {
+        usbSetInterrupt((unsigned char*)&reports[reportReadIndex], sizeof(sizeof(inputReport30_t)));
+    }
 }
