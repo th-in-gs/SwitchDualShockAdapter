25607b526c11912f3bdd82122d6d157354914b65
Jamie Montgomerie <jamie@montgomerie.net>
2022-12-23 15:09:26 -0800

    V-USB working with 12.8MHz calibration. Data pin connections changed in V-USB configuration, and calibration routines included. main.cpp updated.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,53 @@
 #include <Arduino.h>
 
+extern "C" {
+    #include <usbdrv/usbdrv.h>
+
+    // We declare this to be used by V-USB's 'osccal.h' oscilator calibration 
+    // routine.
+    uchar lastTimer0Value = 0;
+}
+
+usbMsgLen_t usbFunctionSetup(uchar data[8])
+{
+    return 0;
+}
+
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
+
+    // Disable interrupts for USB reset.
+    noInterrupts();         
+
+    // Initialize V-USB.
+    usbInit(); 
+
+    // V-USB wiki (http://vusb.wikidot.com/driver-api) says:
+    //      "In theory, you don't need this, but it prevents inconsistencies 
+    //      between host and device after hardware or watchdog resets."
+    usbDeviceDisconnect(); 
+    delay(250); 
+    usbDeviceConnect();
+    
+    // Enable interrupts again.
+    interrupts();           
+}
+
+// Call regularly to blink the LED every 1 second.
+static void heartbeat()
+{
+    static unsigned long lastBlink = 0;
+
+    unsigned long timeNow = millis();
+    if(timeNow - lastBlink >= 1000) {
+        lastBlink = timeNow;
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    }
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    heartbeat();
+    usbPoll();
 }
