5ede74be0b99eda951cd65e4b43376ebf4931fc3
Jamie Montgomerie <jamie@montgomerie.net>
2023-01-10 18:01:24 -0800

    Fake USB HID controller that simulates 16 buttons and two analog sticks.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,59 @@
 
 extern "C" {
     #include <usbdrv/usbdrv.h>
+    #include "descriptors.h"
 
-    // We declare this to be used by V-USB's 'osccal.h' oscillator calibration 
+    // We declare this to be used by V-USB's 'osccal.h' oscilator calibration 
     // routine.
-    uchar lastTimer0Value = 0;
+    uint8_t lastTimer0Value = 0;
 }
+
+
+static GameControllerInputReport sReports[2] = { 0 };
+volatile static uint8_t sReportReadIndex = 0;
+
+static uint8_t sIdleRate;
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
-    return 0;
+    usbRequest_t* rq = (usbRequest_t*)data;
+
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {
+        if(rq->bRequest == USBRQ_HID_GET_REPORT) {
+            if(rq->wValue.bytes[0] == 0x42) {
+                usbMsgPtr = (typeof(usbMsgPtr))(&sReports[sReportReadIndex]);
+                return sizeof(*sReports);
+            }
+        } else if(rq->bRequest == USBRQ_HID_GET_IDLE) {
+            usbMsgPtr = &sIdleRate;
+            return 1;
+        } else if(rq->bRequest == USBRQ_HID_SET_IDLE) {
+            sIdleRate = rq->wValue.bytes[1];
+        }
+    }
+
+    return 0; /* default for not implemented requests: return no data back to host */
+}
+
+static boolean sLedIsOn = false;
+
+static void prepareReport()
+{
+    static uint16_t buttonCounter = 1;
+    uint8_t nextReportIndex = sReportReadIndex == 1 ? 0 : 1;
+    GameControllerInputReport *report = &sReports[nextReportIndex];
+    report->reportId = 0x42;
+
+    uint8_t fakeButtons = (uint8_t)(buttonCounter >> 2);
+    report->buttons1to7 = fakeButtons;
+    report->buttons8to16 = 0xff - fakeButtons;
+    report->leftStickX = fakeButtons;
+    report->leftStickY = 0xff - fakeButtons;
+    report->rightStickX = 0xff - fakeButtons;
+    report->rightStickY = fakeButtons;
+
+    ++buttonCounter;
+    sReportReadIndex = nextReportIndex;
 }
 
 void setup()
@@ -35,19 +79,24 @@ void setup()
 }
 
 // Call regularly to blink the LED every 1 second.
-static void heartbeat()
+static void ledHeartbeat()
 {
-    static unsigned long lastBlink = 0;
+    static unsigned long lastBeat = 0;
 
     unsigned long timeNow = millis();
-    if(timeNow - lastBlink >= 1000) {
-        lastBlink = timeNow;
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if(timeNow - lastBeat >= 1000) {
+        lastBeat = timeNow;
+        sLedIsOn = !sLedIsOn;
+        digitalWrite(LED_BUILTIN, sLedIsOn);
     }
 }
 
 void loop()
 {
-    heartbeat();
+    ledHeartbeat();
     usbPoll();
+    if(usbInterruptIsReady()) {
+        prepareReport();
+        usbSetInterrupt((unsigned char*)&sReports[sReportReadIndex], sizeof(*sReports));
+    }
 }
